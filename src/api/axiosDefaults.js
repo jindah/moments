fab6d11c8961ee5c76493b469e2dbaafdc475c4d
Joonas Timlin <joonas@timlins.se>
2023-12-16 15:54:31 +0000

    add navbar icons to loggedin users

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://drfapinew-d3d8cbc7f33b.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
