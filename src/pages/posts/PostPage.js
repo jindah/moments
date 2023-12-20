a043132c8e9bee92e9c3a542ac4b9e2234671f58
Joonas Timlin <joonas@timlins.se>
2023-12-20 20:53:24 +0000

    Add popular profiles component

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -11,6 +11,10 @@ import Post from "./Post";
 
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Comment from "../comments/Comment";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
+import Asset from "../../components/Asset";
 
 function PostPage() {
     const { id } = useParams();
@@ -22,11 +26,12 @@ function PostPage() {
     useEffect(() => {
       const handleMount = async () => {
         try {
-          const [{ data: post }] = await Promise.all([
+          const [{data: post}, {data: comments}] = await Promise.all([
             axiosReq.get(`/posts/${id}`),
+            axiosReq.get(`/comments/?post=${id}`)
           ]);
           setPost({ results: [post] });
-          console.log(post);
+          setComments(comments);
         } catch (err) {
           console.log(err);
         }
@@ -53,6 +58,28 @@ function PostPage() {
         ) : comments.results.length ? (
           "Comments"
         ) : null}
+        {comments.results.length ? (
+          <InfiniteScroll 
+          children={
+            comments.results.map(comment => (
+              <Comment
+              key={comment.id}
+              {...comment}
+              setPost={setPost}
+              setComments={setComments}
+              />
+            ))
+          }
+          dataLength={comments.results.length}
+          loader={<Asset spinner />}
+          hasMore={!!comments.next}
+          next={() => fetchMoreData(comments, setComments)}
+          />
+        ) : currentUser ? (
+          <span>No comments yet, share your toughs!</span>
+        ) : (
+          <span>No comments... yet.</span>
+        )}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
